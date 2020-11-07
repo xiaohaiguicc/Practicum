a98721a82498d95d6f726b970a59247bb3dd3eab
amdacccache <amdaccache@gmail.com>
2020-11-07 14:22:18 -0800

    Update addActor.sql

--- addActor.sql
+++ addActor.sql
@@ -8,14 +8,31 @@ BEGIN
     (nm002, Dianna Agron, 1986, NULL),
     (nm003, Letitia Wright, 1993, NULL)
   INSERT INTO name_basic_primaryProfession(nconst)
-  (nm001),
-  (nm002),
-  (nm003),
+  VALUES
+    (nm001),
+    (nm002),
+    (nm003),
   INSERT INTO primaryProfession(primaryProfession),
   VALUES 
-    (actress)
-    (actress)
-    (actress)
+    (actress),
+    (actress),
+    (actress),
+  INSERT INTO title_basic(tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runTimeMinutes),
+  VALUES
+    (tt001, "tvEpisode", "The Queen's Gambit", "The Queen's Gambit", 1, 2020, NULL, 52),
+    (tt002, "tvEpisode", "Glee", "Glee", 0, 2012, NULL, 45),
+    (tt003, "movie", "Black Panther", "Black Panther", 0, 2018, NULL, 134),
+  INSERT INTO title_name(titleNameId, tconst, nconst),
+  VALUES 
+    (5, tt001, nm001),
+    (6, tt002, nm002),
+    (7, tt003, nm003),
+  INSERT INTO title_akas(),
+  VALUES 
+    (),
+  INSERT INTO principal(),
+  VALUES 
+    (),
 END //
 
 DELIMITER ;
