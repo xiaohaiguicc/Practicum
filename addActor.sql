d17abc2484fd356f3441b6efe9c605c7d6e31013
amdacccache <amdaccache@gmail.com>
2020-11-07 14:13:38 -0800

    Update addActor.sql

--- addActor.sql
+++ addActor.sql
@@ -2,11 +2,15 @@ DELIMITER //
 
 CREATE PROCEDURE addActor()
 BEGIN
-  INSERT INTO name_basic(primaryName, birthYear, deathYear),
+  INSERT INTO name_basic(nconst, primaryName, birthYear, deathYear),
   VALUES 
-    (Anya Taylor Joy, 1996, NULL),
-    (Dianna Agron, 1986, NULL),
-    (Letitia Wright, 1993, NULL)
+    (nm001, Anya Taylor Joy, 1996, NULL),
+    (nm002, Dianna Agron, 1986, NULL),
+    (nm003, Letitia Wright, 1993, NULL)
+  INSERT INTO name_basic_primaryProfession(nconst)
+  (nm001),
+  (nm002),
+  (nm003),
   INSERT INTO primaryProfession(primaryProfession),
   VALUES 
     (actress)
