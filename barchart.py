40afcd5ac20fdd511786600a76776ba8ea75a009
amdacccache <amdaccache@gmail.com>
2020-11-12 13:52:53 -0800

    Update barchart.py

--- barchart.py
+++ barchart.py
@@ -7,13 +7,14 @@
 import matplotlib.pyplot as plt
 
 Query = ['Pre-Index', 'Post Index']
-Execution = [0.0068, 0.00032]
+Execution = [6.8, 0.32]
 
 plt.bar(Query, Execution)
 plt.title('Query Execution Time')
 plt.xlabel('Query')
-plt.ylabel('Execution Time (seconds)')
+plt.ylabel('Execution Time (miliseconds)')
 plt.show()
+
 
 
 # In[ ]:
